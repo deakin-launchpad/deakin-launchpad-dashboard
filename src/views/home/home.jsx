ade44d3783a4b92e0b24dda8d95c0afda8cacdef
Nirav Bhimani <niravbhimani53@gmail.com>
2018-11-16 14:58:37 +1100

    homeComponent: Add 4  cards

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from 'images/deakin-logo.svg'
 import Header from 'components/header.jsx'
+import Card from 'components/card.jsx'
 
 class Home extends Component {
   render() {
@@ -9,6 +10,10 @@ class Home extends Component {
          <Header />
              <header className="App-header">
         </header>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     );
   }

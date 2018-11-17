39c9347e10ca6f2ee58910d734ef99345dcbcec2
Nirav Bhimani <niravbhimani53@gmail.com>
2018-11-17 15:11:05 +1100

    cardComponent: Remove login check

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,27 +1,9 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedState: false,
-    }
-  }
-
-  // click function to check login state
-  // if logged in then setState to enable card click modal
-  cardClick = () => {
-    if(!this.state.isLoggedState) {
-      console.log(this.state.isLoggedState);
-    } else {
-      console.log(this.state.isLoggedState);
-    }
-  }
-
   render() {
     return (
-      <div className="row">
-        <div className="col s12 m6" onClick={this.cardClick}>
+        <div className="col s12 m6">
           <div className="card blue-grey darken-1">
             <div className="card-content white-text">
               <span className="card-title">
@@ -37,7 +19,6 @@ class Card extends Component {
             </div>
           </div>
         </div>
-      </div>
     );
   }
 }

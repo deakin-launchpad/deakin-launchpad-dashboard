eabb0c3ee06e5221c35417d4d38e43526690f276
Nirav Bhimani <niravbhimani53@gmail.com>
2018-11-17 21:40:23 +1100

    homeComponent: Update Login and modal check function

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,34 +1,41 @@
 import React, { Component } from 'react';
 import logo from 'images/deakin-logo.svg'
 import Header from 'components/header.jsx'
-import Card from 'components/card.jsx'
+import Card from 'components/card.jsx';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedState: false,
-    }
+      // If isLoggedState is false Modal will not pop-up
+      // Otherwise Modal OnClick: true state
+      isLoggedIn: true,
+      modalStatus: false,
+    };
   }
 
   componentDidMount() {
-    if(!this.state.isLoggedState) {
-      console.log(this.state.isLoggedState);
-    } else {
-      console.log(this.state.isLoggedState);
+    if(this.state.isLoggedIn) {
+      this.setState({ modalStatus: true}, () => {
+          console.log(this.state.modalStatus);
+      }) 
     }
   }
-  
+
   render() {
     return (
       <div className="Home">
          <Header />
              <header className="App-header">
         </header>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <div className="row">
+          <Card data={this.state.modalStatus} />
+          <Card data={this.state.modalStatus} />
+        </div>
+        <div className="row">
+          <Card data={this.state.modalStatus} />
+          <Card data={this.state.modalStatus} />
+        </div>
       </div>
     );
   }

14465549b9d1771a265da1faeff68990800120b9
Nirav Bhimani <niravbhimani53@gmail.com>
2018-11-17 15:05:29 +1100

    homeComponent: Add login status check

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -4,6 +4,21 @@ import Header from 'components/header.jsx'
 import Card from 'components/card.jsx'
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedState: false,
+    }
+  }
+
+  componentDidMount() {
+    if(!this.state.isLoggedState) {
+      console.log(this.state.isLoggedState);
+    } else {
+      console.log(this.state.isLoggedState);
+    }
+  }
+  
   render() {
     return (
       <div className="Home">

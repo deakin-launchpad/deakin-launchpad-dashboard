335c407344aa47ad64cb1377d758c00b4f4e963d
Nirav Bhimani <niravbhimani53@gmail.com>
2018-11-18 11:39:20 +1100

    cardComponent: Pass id to each modal

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -6,13 +6,13 @@ class Card extends Component {
     let status = this.props.modalStatus;
     var statusCheck = () => {
       if(status) {
-        return <Modal data_modal={this.props.data_modal}/>;
+        return <Modal data_modal={this.props.data_modal} id={this.props.id} />;
       }
     };
     return (
       <div className={"card_" + this.props.id}>
         <div className="col s12 m6">
-          <div className="card blue-grey darken-1 modal-trigger" data-target="modal1">
+          <div className="card blue-grey darken-1 modal-trigger" data-target={"modal_" + this.props.id}>
             <div className="card-content white-text">
               <span className="card-title">
                 {this.props.data_card.title}
